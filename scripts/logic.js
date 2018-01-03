9302513ff97e59658116b3d6e493a387a0958ba1
Roberto Ortega <berto.ort@gmail.com>
2018-01-02 17:00:13 -0700

    ✨ Add logic solution

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,14 +1,26 @@
 function fireMissle(displayBoard, board, coordinates) {
+  const misslePosition = board[coordinates.y][coordinates.x]
+  displayBoard[coordinates.y][coordinates.x] = misslePosition > 1 ? hit : miss
   return displayBoard
 }
 
 function generateComputerMove(board) {
-  return {
-    x: 0,
-    y: 0
+  for(let y = 0; y < board.length; y++) {
+    for(let x = 0; x < board[y].length; x++) {
+      if (board[y][x] !== 1 && board[y][x] !== -1) {
+        return {x, y}
+      }
+    }
   }
 }
 
 function isWon(board) {
-  return false
+  const winTotal = 17
+  const sum = board.reduce((sum, row) => {
+    row.forEach(box => {
+      if (box === -1) sum++
+    })
+    return sum
+  }, 0)
+  return sum === winTotal
 }
